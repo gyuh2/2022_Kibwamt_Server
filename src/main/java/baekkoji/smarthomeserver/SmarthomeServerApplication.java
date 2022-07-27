6368eae5b6c1b3803b83c00199e29073cba44d0a
chae0 <chae0_98@naver.com>
2022-07-27 13:41:46 +0900

    20220711 ServerIP find complete

--- src/main/java/baekkoji/smarthomeserver/SmarthomeServerApplication.java
+++ src/main/java/baekkoji/smarthomeserver/SmarthomeServerApplication.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,12 +15,29 @@ import java.util.Date;
 public class SmarthomeServerApplication
 {
 
+    private static String getServerip()
+    {
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+        }catch(UnknownHostException e){
+            e.printStackTrace();
+        }
+        if (local == null){
+            return "";
+        }
+        else {
+            return local.getHostAddress();
+        }
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(SmarthomeServerApplication.class, args);
         Sensor sensor = new Sensor();
-        
-        String day_join=""; // 오늘날짜 저장할 변수
+        String ipString = getServerip();
+        System.out.println(ipString);
+        String day_join; // 오늘날짜 저장할 변수
         Date today = new Date();
         SimpleDateFormat today_format = new SimpleDateFormat("yyyyMMdd");
         day_join = today_format.format(today);
