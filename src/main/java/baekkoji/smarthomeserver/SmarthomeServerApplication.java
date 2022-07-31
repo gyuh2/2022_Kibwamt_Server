939d9349b8892505e25dac87500452c5b671e138
chae0 <chae0_98@naver.com>
2022-07-31 16:45:47 +0900

    20220731 1. "SQL Insert" Add - you would execute just once.
    2. And I tried to send response to Arduino, though it was failed. Pls some help.
    
    Ps. You should change date in 'Sensor.java'; urlBuilder_tmp ~~~date=(on thisDate)

--- src/main/java/baekkoji/smarthomeserver/SmarthomeServerApplication.java
+++ src/main/java/baekkoji/smarthomeserver/SmarthomeServerApplication.java
@@ -19,19 +19,21 @@ public class SmarthomeServerApplication
     public static void main(String[] args) throws SQLException {
 
         String url = "jdbc:mysql://localhost:3307/SmartHome";
-        String userName = "chaeyoung";
+        String userName = "root";
         String password = "1234";
 
         Connection connection = DriverManager.getConnection(url, userName, password);
         Statement statement = connection.createStatement();
-        statement.executeUpdate("insert into users(Name) values('junyoung')");
-        /*ResultSet resultSet = statement.executeQuery("select * from users");
+        //insert가 되는지 test한 코드로 주석을 제거하고 1번만 실행하여 잘 반영 되었는지 확인만 해주세요.//
+        //statement.executeUpdate("insert into users(Name) values('junyoung')");
+        ResultSet resultSet = statement.executeQuery("select * from users");
 
-        resultSet.next();
-        String name = resultSet.getString("name");
-        System.out.println(name);
+        while(resultSet.next()) {
+            String name = resultSet.getString("Name");
+            System.out.println(name);
+        }
 
-        resultSet.close();*/
+        resultSet.close();
         statement.close();
         connection.close();
 
