615b63e5dd050c8a871d8a2dabdfdc7fab921f51
Qnokka <soly0911@naver.com>
2022-08-13 18:13:10 +0900

    20220813_2

--- src/main/java/baekkoji/smarthomeserver/dto/ControlData.java
+++ src/main/java/baekkoji/smarthomeserver/dto/ControlData.java
@@ -5,14 +5,19 @@ import lombok.Data;
 @Data
 public class ControlData {
 
-    private String device;
-    private int angle;
-    private boolean control;
+    private int angle=0;
+    private int AC_temp=0;
+    private int heater_temp=0;
+    private boolean window = false;
+    private boolean heater = false;
+    private boolean AC = false;
+    private boolean airCleaner = false;
+    private boolean airOut = false;
 
     public int controlHome(){
         int result = 0;
         //DB에 저장하고, D 아두이노에 제어 요청을 하고 정상처리 되면 응답을 받아야함.
-        System.out.println(device + ", " +  angle + ", " + control);
+
         result = 1;
         return result;
     }
