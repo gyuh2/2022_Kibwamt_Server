ddd2deda03173042bbd0fb491e9cce4b45970ec4
chae0 <chae0_98@naver.com>
2022-08-17 15:47:56 +0900

    20220815
    Arduino do get controlData from Server. - Test1

--- src/main/java/baekkoji/smarthomeserver/controller/IoTController.java
+++ src/main/java/baekkoji/smarthomeserver/controller/IoTController.java
@@ -17,11 +17,11 @@ public class IoTController {
 
     @PostMapping("/set-data")
     public @ResponseBody String setSensorData(@RequestBody Sensor sensor) throws SQLException { //아두이노 -> 서버 (센서값 전달)
-        sensor.APIData();
-        sensor.setDataAll(); //DB에 저장하는 코드
-        status = sensor.ChangeStatus(); //데이터 처리한 값 저장.
+        sensor.APIData(); //공공데이터 참조하여 멤버 변수에 저장.
+        sensor.setDataAll(); //DB에 멤버변수 모두 저장
+        status = sensor.ChangeStatus(); //데이터 분석하여 제어할 값 저장.
         System.out.println(sensor); //콘솔 출력
-        System.out.println(status);
+        System.out.println(status); //제어 Case 출력
         return status; // 아두이노 request에 대한 응답.
     }
 
